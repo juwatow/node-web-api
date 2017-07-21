a05800e4fe72a6e02a89cc64c7b70fa8320bd464
Jeanne <jeanneu@careercruising.com>
2017-07-21 13:02:31 -0400

    Add Caching with Redis

--- student_routes.js
+++ student_routes.js
@@ -1,5 +1,8 @@
 var _= require('lodash');
 var Student = require('./student_model.js')
+var redis = require('redis');
+
+var client = redis.createClient('6379', '127.0.0.1');
 
 module.exports = function(app){
 
@@ -16,11 +19,22 @@ module.exports = function(app){
 
     /* Read */
     app.get('/students', function(req, res) {
-        Student.find(function(err, students) {
-            if(err) {
-                res.json({info: 'error during student finding', error: err});
+        client.get('STUDENTS', function(error, students) {
+            if(error) {throw error;};
+            if(students) {
+                res.json({info: 'students found successfuly from the cache', data: JSON.parse(students)});
+            } else {
+                Student.find(function(err, students) {
+                    if(err) {
+                        res.json({info: 'error during student finding', error: err});
+                    }
+                    res.json({info: 'students found successfuly from the DB', data: students});
+                    //client.set('STUDENTS', JSON.stringify(students), function(error) {
+                    client.setex('STUDENTS', 10, JSON.stringify(students), function(error) {
+                        if(error) {throw error;};
+                    });
+                });
             }
-            res.json({info: 'students found successfuly', data: students});
         });
     });
 
